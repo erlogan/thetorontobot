c66a67d121b7f14da73c8646e4503b055199e46a
Eric Logan <erlogan@gmail.com>
2016-10-24 22:20:42 -0400

    Sidebar auto-updater
    - Reimplement the 'hot sister' script in snoocore to update the
      /r/toronto sidebar with the top 5 posts from /r/askto
    - Bump app user-agent version

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var moment = require('moment-timezone');
 var fs = require('fs');
 var dailyOverview = require('./dailyoverview.js');
+var sidebarUpdate = require('./sidebar.js');
 var functions = require('./functions.js');
 
 var today;
@@ -10,13 +11,14 @@ var redditPollingLoop = function () {
 	today = moment().tz("America/Toronto");
 	var utcDate = fs.readFileSync("yesterday", "utf-8");
 	yesterday = moment.utc(moment.unix(functions.stripWhiteSpaceAndNewLines(utcDate))).tz("America/Toronto");
+    sidebarUpdate();
 	if (today.isAfter(yesterday.tz("America/Toronto"), 'day')){
 		dailyOverview();
 		console.log(moment.tz("America/Toronto").format() +  ': New Post Made!');
 	}
 	else
 		console.log(moment.tz("America/Toronto").format() +  ': Not next day. Waiting 5min. - Yesterday: ' + yesterday.format() + ' Today: ' + today.format());
-	setTimeout(redditPollingLoop, 1000 * 60 * 5); //5min
+	setTimeout(redditPollingLoop, 1000 * 60 * 15); //15min
 };
 
 redditPollingLoop();
---
+++ sidebar.js
@@ -0,0 +1,52 @@
+var Snoocore = require('snoocore');
+var config = require('./config.js');
+
+module.exports = function () {
+    var reddit = new Snoocore({
+        userAgent: '/u/thetorontobot TheTorontoBot@0.0.2',
+        oauth: {
+            type: 'script',
+        key: config.key,
+        secret: config.secret,
+        username: config.username,
+        password: config.password,
+        scope: ['read','modconfig']
+        }
+    });
+
+    var generatePostTable = function (posts) {
+        var postText = "";
+        var newLine = "\r\n";
+
+        posts.forEach(function (post) {
+            postText += newLine;
+            postText += "1. [" + post.data.title + "](" + post.data.url + ")";
+        });
+
+        return postText + newLine;
+    };
+
+    reddit('/r/toronto/about/edit.json').get().then(function(result) {
+        var data = result.data;
+
+        // MUST specify these extra options.
+        // unfortunately /about/edit.json does not provide all the
+        // parameters for the /api/site_admin call
+
+        data.api_type = 'json'; // must be the string 'json'
+
+        // NEEDED ALIASES
+        data.sr = data.subreddit_id; // add .sr alias for subreddit_id
+        data.link_type = data.content_options; // add .link_type alias for content_options
+        data.type = data.subreddit_type; // add .type alias for subreddit_type
+
+        // Finally, make any actual changes needed
+        reddit('/r/askTO/hot').listing({limit: 5}).then(
+            function(slice){ 
+                data.description = data.description.replace(/(\[\]\(\/questions-start\)\n)[\s\S]*?(\n\[\]\(\/questions-end\))/m,"$1"+generatePostTable(slice.children)+"$2");
+                data.description = data.description.replace(/amp;/g,""); // workaround -- telling snoocore not to decode entities doesn't work apparently
+                return reddit('/api/site_admin').post(data);
+            }
+        );
+    });
+}
