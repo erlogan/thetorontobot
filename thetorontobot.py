3ad9901e94328117798d374fafc10ed53de05af7
Eric Logan <erlogan@gmail.com>
2023-05-06 17:57:02 -0400

    Add logging, cleanup, actually post to /r/toronto

--- thetorontobot.py
+++ thetorontobot.py
@@ -1,21 +1,23 @@
+import logging
 import praw
 import re
 import schedule
 import time
 
-import sys
-
 def main():
     reddit = praw.Reddit("thetorontobot", config_interpolation="basic")
 
-    schedule.every(1).seconds.do(sidebar, reddit=reddit)
+    schedule.every(15).minutes.do(sidebar, reddit=reddit)
     schedule.every().day.at("00:00", "America/Toronto").do(dailypost, reddit=reddit)
+
+    schedule.run_all()
+
     while True:
         schedule.run_pending()
-        time.sleep(1)
+        time.sleep(60)
 
 def sidebar(reddit):
-    sidebar = reddit.subreddit("toronto2").wiki["config/sidebar"].content_md
+    sidebar = reddit.subreddit("toronto").wiki["config/sidebar"].content_md
     newtext = "\r\n"
     for submission in reddit.subreddit("askTO").hot(limit=7):
         if (submission.stickied):
@@ -30,16 +32,12 @@ def sidebar(reddit):
             title.replace(u'\u201D','"')
             newtext += "1. [" + title + "](https://www.reddit.com" + submission.permalink + ")\r\n\r\n"
     sidebar = re.sub(r"(\[\]\(\/questions-start\)\n)[\s\S]*?(\n\[\]\(\/questions-end\))", "\g<1>"+newtext+"\g<2>", sidebar, flags=re.M)
-    #print("New sidebar:")
-    #print("@@@@@")
-    #print(sidebar)
-    #print("@@@@@")
-    reddit.subreddit("toronto2").wiki["config/sidebar"].edit(content=sidebar, reason="update with the latest from /r/askTO")
-    sys.exit(0)
+    reddit.subreddit("toronto").wiki["config/sidebar"].edit(content=sidebar, reason="update with the latest from /r/askTO")
+    logging.info('Sidebar updated with:\n'+newtext)
 
 def dailypost(reddit):
-    print("Time to make the daily post")
-    print("The time is" + time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()))
+    logging.info("Time to make the daily post")
 
 if __name__ == '__main__':
+    logging.basicConfig(format='[%(asctime)s]  %(message)s', datefmt='%Y-%m-%d %H:%M:%S %Z', level=logging.INFO)
     main()
